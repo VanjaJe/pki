6a29a14e8c7df2147cc93bb9637966d9663e5ff8
Sonja Baljicki <sonjabaljicki2002@gmail.com>
2024-04-18 21:46:14 +0200

    added endpoints for getting certificates and certificate requests

--- src/main/java/com/example/PKI/controller/CertificateController.java
+++ src/main/java/com/example/PKI/controller/CertificateController.java
@@ -1,11 +1,25 @@
 package com.example.PKI.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.PKI.domain.Certificate;
+import com.example.PKI.domain.enums.CertificateType;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/certificate")
 public class CertificateController {
+    @GetMapping
+    public ResponseEntity<Collection<Certificate>> getAll () {
+        Collection<Certificate>certificates=new ArrayList<>();
+        return new ResponseEntity<Collection<Certificate>>(certificates,HttpStatus.OK);
+    }
 
 }
